472b19ef66cea82f0a51d6ba131527399717c05e
SC-SAKIB <sakibhossain13@gmail.com>
2026-01-07 17:09:01 +0600

    Add files via upload
    My first dictionary code

---
+++ 4th lecture.py
@@ -0,0 +1,15 @@
+#Dictionary and sets
+info = {
+    "name" : "sakib",
+    "age" : 25,
+    "Subjects" : {
+        "Physics" : 97,
+        "math" : 85,
+        "English" : 80
+    }
+}   
+info["Subjects"].update({"Bangla": 75})
+print(info) 
+
+#Set in python
+
