c8faf9312ea40a8505ab7a8e01fca3a030177064
SC-SAKIB <sakibhossain13@gmail.com>
2026-01-29 12:07:10 +0600

    Organizing all lecture files

---
+++ 5th Lecture_Loops.py
@@ -0,0 +1,17 @@
+#while Loops 
+#iteration here count is I mean variable is iterator 
+count=1
+while count<=5:
+    print("HELLO")
+    count+=1
+print(count)
+ 
+i=1
+while i<=5:
+    print(i)
+    i+=1
+print("loop ended")
+i=5
+while i>=1:
+    print(i)
+    i-=1
